03e592cd4a21d9aa0b838c0183667ec233411bc7
seniorcat <igrlvr@gmail.com>
2020-10-11 21:48:40 +0300

    added choice sort

--- app/ArraySorter.php
+++ app/ArraySorter.php
@@ -5,6 +5,12 @@ namespace app;
 class ArraySorter
 {
 
+    /**
+     * Сортировка вставками
+     * Функция сортирует массив по возрастанию
+     * @param array $a
+     * @return array
+     */
     public function insertSort(array $a)
     {
         for ($i = 1; $i < count($a); $i++) {
@@ -14,9 +20,37 @@ class ArraySorter
                 $a[$top - 1] = $a[$top];
                 $a[$top] = $tmp;
                 $top -= 1;
+                unset($tmp);
             }
         }
 
         return $a;
     }
+
+    /**
+     * Сортировка выбором
+     * Функция сортирует массив по возрастанию
+     * @param array $a
+     * @return array
+     */
+    public function choiceSort(array $a)
+    {
+        $n = count($a) - 1;
+        
+        for ($i = 0; $i < $n; $i++) {
+            $min = $i;
+            for($j=$i+1; $j<count($a); $j++) {
+                if ($a[$j]<$a[$min]) {
+                    $min = $j;
+                }
+            }
+            $tmp = $a[$min];
+            $a[$min] = $a[$i];
+            $a[$i] = $tmp;
+            unset($tmp);
+        }
+
+        return $a;
+    }
+
 }
--- tests/ArraySorterTest.php
+++ tests/ArraySorterTest.php
@@ -9,4 +9,10 @@ class ArraySorterTest extends TestCase
         $arraySort = new app\ArraySorter();
         $this->assertSame([1,2,3,4,5], $arraySort->insertSort([5,3,1,4,2]));
     }
+
+    public function testChoiceSort()
+    {
+        $arraySort = new app\ArraySorter();
+        $this->assertSame([1,2,3,4,5], $arraySort->choiceSort([5,3,1,4,2]));
+    }
 }
